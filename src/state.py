7478b97cc840b32bd56f847e34b101898525456e
Viliam Vadocz <viliam.vadocz@gmail.com>
2026-02-05 15:58:38 +0100

    test: check state invariants

--- src/state.py
+++ src/state.py
@@ -5,6 +5,7 @@ import jax.numpy as jnp
 from jax.experimental import checkify  # https://docs.jax.dev/en/latest/debugging/checkify_guide.html
 
 import zobrist
+from bitboard import mask_to_bb
 from consts import DEFAULT_CAPS, DEFAULT_SIZE, DEFAULT_STONES, MAX_STACK_SIZE, check_size
 
 
@@ -28,3 +29,60 @@ class State(NamedTuple):
     black_stones: jax.Array = jnp.uint8(DEFAULT_STONES)
     white_caps: jax.Array = jnp.uint2(DEFAULT_CAPS)
     black_caps: jax.Array = jnp.uint2(DEFAULT_CAPS)
+
+
+def check_invariants(state: State) -> None:
+    check_size(state.size)
+
+    # Calculate number of pieces on the board from stacks
+    stack_heights = MAX_STACK_SIZE - jax.lax.clz(state.stacks)
+    white_pieces = jax.lax.population_count(state.stacks) - 1
+    black_pieces = stack_heights - white_pieces
+    white_pieces = jnp.sum(white_pieces)
+    black_pieces = jnp.sum(black_pieces)
+
+    # Figure out how many pieces are capstones
+    caps = state.road_bb & state.noble_bb
+    white_caps = jax.lax.population_count(caps & state.white_bb)
+    black_caps = jax.lax.population_count(caps & state.black_bb)
+    white_flats = white_pieces - white_caps
+    black_flats = black_pieces - black_caps
+
+    # Check reserves consistent with stacks
+    checkify.check(state.white_stones == state.starting_stones - white_flats, "incorrect white stone reserves")
+    checkify.check(state.black_stones == state.starting_stones - black_flats, "incorrect black stone reserves")
+    checkify.check(
+        state.white_caps.astype(jnp.uint8) == state.starting_caps - white_caps, "incorrect white cap reserves"
+    )
+    checkify.check(
+        state.black_caps.astype(jnp.uint8) == state.starting_caps - black_caps, "incorrect black cap reserves"
+    )
+
+    walls = state.noble_bb & ~state.road_bb
+    flats = ~state.noble_bb & state.road_bb
+    not_empty = stack_heights > 0
+    black = (state.stacks & 0b1) == 0
+    white = not_empty & ~black
+
+    # Check bitboards consistent with reserves and stacks
+    checkify.check(state.white_bb == mask_to_bb(white), "incorrect white bitboard")
+    checkify.check(state.black_bb == mask_to_bb(black), "incorrect black bitboard")
+    checkify.check(state.white_bb & state.black_bb == 0, "white and black bitboard overlap")
+
+    any_color = state.white_bb | state.black_bb
+    any_piece = state.road_bb | state.noble_bb
+    checkify.check(any_color == any_piece, "inconsistent color and piece bitboards")
+
+    wall_count = jax.lax.population_count(walls)
+    flat_count = jax.lax.population_count(flats)
+    checkify.check(jax.lax.population_count(caps) == white_caps + black_caps, "incorrect cap reserves")
+    checkify.check(wall_count + flat_count == white_flats + black_flats, "incorrect stone reserves")
+
+    checkify.check(state.zobrist_hash == zobrist.get_hash(state), "incorrect hash")
+
+
+def test_invariants() -> None:
+    state = State()
+    err: checkify.Error
+    err, _ = jax.jit(checkify.checkify(check_invariants, errors=checkify.all_checks))(state)
+    err.throw()
